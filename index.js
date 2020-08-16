8e4c12e9514b9e8609425d139ff4efec9c471bde
Tellur89 <62219709+Tellur89@users.noreply.github.com>
2020-08-16 20:03:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 $(".btn_menu").on("click", function(){
     $(".btn_menu").toggleClass("btnc");
-    $(".sidebar").toggleClass("side");
-    $(".container-fluid").toggleClass("container_small");
     $(".title").toggleClass("title-move");
-    $(".icons_social").toggleClass("icons_social-move");
-    $(".row").toggleClass("row_small");
+    $(".icons_social").toggleClass("icons_social_small");
     $(".photo_background").toggleClass("photo_background_small");
-    $(".photo_background1").toggleClass("photo_background1_small");
+    $(".photo_back").toggleClass("photo_background1_small");
     $(".link").toggleClass("link_small");
+    $(".row").toggleClass("row_small");
+    $(".row_venue").toggleClass("row_venue_small");
+    $(".row_footer").toggleClass("row_footer_small");
+    $(".book_link").toggleClass("book_link_small");
+    $(".footer").toggleClass("row_small");
+    $(".slide_text_right").toggleClass("slide_text_right_small");
+    $(".row_about").toggleClass("row_about_small");
+    
 });
 
 var slideIndex = 0;
@@ -24,5 +29,11 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000);
 }
+
+$(document).ready(function(){
+  $("#sidebarCollapse").on("click", function(){
+      $("#sidebar").toggleClass("active");
+  });
+});
 
 
